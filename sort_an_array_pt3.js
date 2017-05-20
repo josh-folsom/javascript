21b0b71641241ea6f2cacb05685c04a78e358b57
josh-folsom <folsom.josh@gmail.com>
2017-05-20 17:24:53 -0500

    committing some JS array exercises

--- sort_an_array_pt3.js
+++ sort_an_array_pt3.js
@@ -5,32 +5,29 @@ var arr = [
   [2, 4, 6, 8],
   [3, 6]
 ];
-// Sort the array by the sum of each inner array.
-// For the above example, the respective sums for
-// each inner array is 8, 20, and 9. Therefore,
-// the solution should be:
 
-// [
-//   [1, 3, 4],
-//   [3, 6],
-//   [2, 4, 6, 8]
-// ]
 
-// arr.sort(function (x, y, c) {
-//   if (x + y + c ) { return 1; }
-//   else if (x < y) { return -1; }
-//   return 0;
-// });
-
-// a.reduce(function (x, y) {
-//   return a + b;
-// });
-
-var ham = arr.sort(function (x, y) {
-  if (x > y) { return 1; }
-  else if (x < y) { return -1; }
-  return 0;
+function mySum (myArray) 
+{
+  return myArray.reduce(function (x, y) {
+  return x + y;
 });
+}
 
-console.log(ham);
+function compareSum(a, b)
+{
+  if (mySum(a)<mySum(b)) 
+  {
+    return -1;
+  }
+  if (mySum(a)>mySum(b)) 
+  {
+    return 1;
+  }
+  
+  return 0;
+}
+
+console.log(arr.sort(compareSum));
+
 
